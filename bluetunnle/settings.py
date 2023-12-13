0f01d5b337f0242db79593a7c3f4080ce621f208
Srivardhan <srivardhan.singh.rathore@gmail.com>
2023-12-14 00:45:36 +0530

    DEBUG=False

--- bluetunnle/settings.py
+++ bluetunnle/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pk@2(37@7(4t+pmbb)2)6$g&o%&6zs3@n9rv+h34_ya+_9c165'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
